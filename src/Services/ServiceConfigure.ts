599af03934c762fa772f8af959f0e102e34a8bc7
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-02-27 12:48:55 +0700

    feat(ServiceConfigure): add default level

--- src/Services/ServiceConfigure.ts
+++ src/Services/ServiceConfigure.ts
@@ -1,14 +1,16 @@
 import { IServiceConfigs } from "atomservicescore";
 import { IServiceConfigure } from "./IServiceConfigure";
 
+const DefaultLevel = "public";
+
 export const ServiceConfigure = (configs: IServiceConfigs): IServiceConfigure => ({
   level: (name) => {
     if (name[0] === "_") {
-      return "public";
+      return DefaultLevel;
     } else if (configs.events !== undefined && configs.events.levels !== undefined) {
-      return configs.events.levels[name] || configs.events.levels._default;
+      return configs.events.levels[name] || configs.events.levels._default || DefaultLevel;
     } else {
-      return "public";
+      return DefaultLevel;
     }
   },
 });
