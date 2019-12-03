1fca3d34eeaf7d9b085463ade9dda162a103f2db
Panit Tuangsuwan <panit@Panits-MacBook-Pro-2.local>
2019-12-03 18:30:59 +0700

    release: v.0.7.0-alpha.38

--- src/Containers/createContainer.ts
+++ src/Containers/createContainer.ts
@@ -1,14 +1,29 @@
-import { IManagedServiceContainer, IServiceContainer } from "atomservicescore";
+import { IManagedService, IManagedServiceContainer, IServiceContainer } from "atomservicescore";
 import { createService } from "../Services/createService";
 
 export const createContainer = (container: IServiceContainer): IManagedServiceContainer =>
   ((CONTAINER): IManagedServiceContainer => {
+    let SERVICES: { [type: string]: IManagedService } = {};
+
     const Container: IManagedServiceContainer = {
       connect: async () => {
         const Services = CONTAINER.Services.map((SERVICE) => createService(SERVICE, CONTAINER));
-
         await Promise.all(Services.map((service) => service.connect()));
+
+        SERVICES = Services.reduce((result, service) => {
+          const type = service.type();
+          result[type] = service;
+
+          return result;
+        }, SERVICES);
       },
+      dispatch: (type) => async (command, listening) => {
+        const service = SERVICES[type];
+
+        return service.dispatch(command, listening);
+      },
+      service: (type) =>
+        SERVICES[type],
     };
 
     Object.freeze(Container);
