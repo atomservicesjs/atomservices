69453a2400aa59f86f7536c98f723f5a65ab3ebf
Panit Tuangsuwan <tpanitte@hotmail.com>
2019-12-11 23:10:23 +0700

    feat(ServiceConfigs)

--- src/Context/Factories/ServiceConfigurateFactory.ts
+++ src/Context/Factories/ServiceConfigurateFactory.ts
@@ -7,9 +7,12 @@ const DefaultProcessType: EventProcessType = "asynchronous";
 export const ServiceConfigurateFactory = {
   create: (configs: IServiceConfigs): IServiceConfigurate => ((Configs): IServiceConfigurate => {
     const ServiceStream: IServiceConfigurate = {
-      allowDynamicVersion: (name) => (Configs.events && Configs.events[name]) ? (Configs.events[name].allowDynamicVersion || DefaultAllowDynamicVersion) : DefaultAllowDynamicVersion,
-      level: (name) => (Configs.events && Configs.events[name]) ? (Configs.events[name].level || (Configs.service && Configs.service.level) || DefaultLevel) : DefaultLevel,
-      processType: (name) => (Configs.events && Configs.events[name]) ? (Configs.events[name].processType || DefaultProcessType) : DefaultProcessType,
+      allowDynamicVersion: (name) => (Configs.events && Configs.events[name]) ?
+        (Configs.events[name].allowDynamicVersion || (Configs.service && Configs.service.allowDynamicVersion) || DefaultAllowDynamicVersion) : DefaultAllowDynamicVersion,
+      level: (name) => (Configs.events && Configs.events[name]) ?
+        (Configs.events[name].level || (Configs.service && Configs.service.level) || DefaultLevel) : DefaultLevel,
+      processType: (name) => (Configs.events && Configs.events[name]) ?
+        (Configs.events[name].processType || (Configs.service && Configs.service.processType) || DefaultProcessType) : DefaultProcessType,
     };
 
     Object.freeze(ServiceStream);
