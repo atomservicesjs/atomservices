37c3766963d93552a961e28f9c7b021499f80795
Panit Tuangsuwan <tpanitte@hotmail.com>
2020-01-16 15:51:03 +0700

    release: v.0.7.0-alpha.68

--- src/Services/core/composeEventProcess.ts
+++ src/Services/core/composeEventProcess.ts
@@ -11,13 +11,17 @@ export const composeEventProcess = (definition: IServiceDefinition): EventStream
   const ServiceStateStores: any = {};
 
   const EventProcess: EventStream.StreamProcessing = async (event, metadata, processAck) => {
-    const EventHandler = EventHandlers.resolve(event);
+    const processType = Definition.ServiceConfigurate.processType(event.name);
 
-    if (EventHandler) {
-      const ServiceContext = composingServiceContext(metadata);
-      const resulting = composeEventResulting(ServiceContext, event);
+    if (processType === "asynchronous") {
+      const EventHandler = EventHandlers.resolve(event);
 
-      await managedEventProcess(EventHandler, ServiceContext, resulting, Notifiers, ServiceStateStores)(Definition, event, metadata);
+      if (EventHandler) {
+        const ServiceContext = composingServiceContext(metadata);
+        const resulting = composeEventResulting(ServiceContext, event);
+
+        await managedEventProcess(EventHandler, ServiceContext, resulting, Notifiers, ServiceStateStores)(Definition, event, metadata);
+      }
     }
 
     await processAck();
--- src/Services/core/connectStream/mapHandlersEvents.ts
+++ src/Services/core/connectStream/mapHandlersEvents.ts
@@ -3,11 +3,8 @@ import { EventStream, IServiceDefinition } from "atomservicescore";
 export const mapHandlersEvents = (definition: IServiceDefinition): Array<{ name: string; level: EventStream.StreamLevel; }> =>
   definition.EventHandlers.reduce((result, { name }) => {
     const level = definition.ServiceConfigurate.level(name);
-    const processType = definition.ServiceConfigurate.processType(name);
-
-    if (processType === "asynchronous") {
-      result.push({ name, level });
-    }
+    // const processType = definition.ServiceConfigurate.processType(name);
+    result.push({ name, level });
 
     return result;
   }, [] as Array<{ name: string; level: EventStream.StreamLevel; }>);
