16ef3eb8e34df42799229b57638ea37e371b7d41
Panit Tuangsuwan <tpanitte@hotmail.com>
2020-01-20 01:40:08 +0700

    release: v.0.7.0-alpha.72

--- src/Commands/ICommandDispatchers.ts
+++ src/Commands/ICommandDispatchers.ts
@@ -1,5 +1,5 @@
 import { DispatchResult, ICommand } from "atomservicescore";
 
 export interface ICommandDispatchers {
-  dispatch: (type: string, command: ICommand, listening?: (data: any) => void) => Promise<DispatchResult.DispatchResultType>;
+  dispatch: (type: string, command: ICommand, listening?: (data: any) => void) => Promise<DispatchResult.IDispatchResult>;
 }
