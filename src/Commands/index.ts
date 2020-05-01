116714c73ffe09ab1ba39d77377473e0392a1db3
Panit Tuangsuwan <tpanitte@hotmail.com>
2020-05-01 12:11:18 +0700

    feat(commands)

--- src/Commands/index.ts
+++ src/Commands/index.ts
@@ -1,12 +1,5 @@
-import { DispatchResult } from "atomservicescore";
-
-export {
-  DispatchResult,
-};
-
 export { Builders } from "./Builders";
 export { combineCommandDispatchers } from "./combineCommandDispatchers";
-// export { composeCommandDispatcher } from "./composeCommandDispatcher";
 export { composeCommandHandlers } from "./composeCommandHandlers";
 export { ICommandDispatcher } from "./ICommandDispatcher";
 export { ICommandDispatchers } from "./ICommandDispatchers";
